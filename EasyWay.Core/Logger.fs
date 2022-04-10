9c475b2b5d1547d5cacb9c3977ec83d6c0b14f48
mikhail.smirnov@bimgen.app <mikhail.smirnov@bimgen.app>
2022-04-10 19:13:44 +0700

    Async added

---
+++ EasyWay.Core/Logger.fs
@@ -0,0 +1,57 @@
+﻿namespace EasyWay.Core.Utils
+
+open System
+open System.IO
+open System.Reflection
+open System.Text
+open NLog
+open NLog.Config
+open NLog.Targets
+
+type Logger private () =
+    let assembly = Assembly.GetCallingAssembly()
+    let name = assembly.GetName().Name
+    static let mutable _instance = Logger()
+    let mutable _logger = LogManager.GetLogger($"{name}.Logger")
+    
+    do
+        let directory    = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+        let logDirectory = Path.Combine(directory, "Logs/")
+        let config       = LoggingConfiguration()
+        
+        if not <| Directory.Exists directory then raise <| DirectoryNotFoundException directory
+        
+        let allFileTarget = new FileTarget("allFileTarget")
+        
+        allFileTarget.FileName         <- Layouts.Layout.FromString $"{logDirectory}Debug.log"
+        allFileTarget.Layout           <- Layouts.Layout.FromString "${longdate} ${uppercase:${level}} ${message}"
+        allFileTarget.Encoding         <- Encoding.Unicode
+        allFileTarget.ArchiveFileName  <- Layouts.Layout.FromString ($"{logDirectory}" + "Debug.${shortdate}.{#}.log")
+        allFileTarget.ArchiveAboveSize <- 16_777_216L
+        allFileTarget.ArchiveEvery     <- FileArchivePeriod.Day
+        allFileTarget.ArchiveNumbering <- ArchiveNumberingMode.Rolling
+        allFileTarget.MaxArchiveFiles  <- 20
+        
+        config.AddTarget(allFileTarget)
+        config.AddRuleForAllLevels(allFileTarget)
+        
+        LogManager.Configuration <- config
+        
+        _logger <- LogManager.GetLogger($"{name}.Logger")
+    
+    member x.Logger
+        with get() = _logger
+        and set value = _logger <- value
+    
+    static member private Instance =
+        if _instance.Logger.Name = Assembly.GetCallingAssembly().GetName().Name then _instance
+        else
+            _instance <- Logger()
+            _instance
+    
+    static member private Log = Logger.Instance.Logger
+    static member Debug(message: String)    = Logger.Log.Debug(message)
+    static member Debug(message: Exception) = Logger.Log.Debug(message)
+    static member Info(message: String)     = Logger.Log.Info(message)
+    static member Info(message: Exception)  = Logger.Log.Info(message)
+
--- EasyWay.Core/Program.fs
+++ EasyWay.Core/Program.fs
@@ -1,40 +1,65 @@
 ﻿module EasyWay.Core.Program
 
+open System
+open EasyWay.Core.Utils
 open Elmish
 open Elmish.WPF
 
-
 type Msg =
-    |SetCount
-    |SetMinus
-    |SetState of string
-    |AddThing
-    |DelThing
-   
-type Model = {Count: int; State:string; ToDoList: string list}
+    | SetCount
+    | SetMinus
+    | SetState of string
+    | SetList of string list
+    | AddThing
+    | DelThing
 
-let init () = { Count = 0
-                State = "10"
-                ToDoList = []}, []
+
+type Model =
+    { Count: int
+      State: string
+      ToDoList: string list }
+
+
+let init () =
+    { Count = 0
+      State = "10"
+      ToDoList = [] }, []
+
+
+let SetAsyncCmd some =
+    fun dispatch ->
+        async {
+            let start = DateTime.Now
+            Logger.Info($"SetAsyncCmd Start: {start}")
+            do! Async.Sleep(3000)
+            dispatch DelThing
+            do! Async.Sleep(2000)
+            dispatch <| SetList some
+            let final = DateTime.Now
+            Logger.Info($"SetAsyncCmd Stop: {start}")
+            Logger.Info($"Total time: {final - start}")
+        } |> Async.StartImmediate
+       
+
 let update msg m =
-    match msg with 
-    |SetCount -> {m with Count = m.Count + 1}, []
-    |SetMinus -> {m with Count = m.Count - 1}, []
-    |SetState v -> {m with State = v}, []
-    |AddThing -> {m with ToDoList = m.State :: m.ToDoList }, []
-    |DelThing -> {m with ToDoList = List.Empty},[]
-    
-    
-   
-let bindings () = 
-    ["Count" |> Binding.oneWay (fun m -> m.Count)
-     "SetCount" |> Binding.cmd SetCount
-     "SetMinus" |> Binding.cmd SetMinus
-     "State" |> Binding.twoWay((fun m -> m.State.ToString()), SetState)
-     "ToDoList" |> Binding.oneWay (fun m -> m.ToDoList)
-     "AddThing" |> Binding.cmd AddThing
-     "DelThing" |> Binding.cmd DelThing]
-    
+    match msg with
+    | SetCount   -> { m with Count = m.Count + 1 }, []
+    | SetMinus   -> { m with Count = m.Count - 1 }, []
+    | SetState v -> { m with State = v }, []
+    | SetList v ->  { m with ToDoList = v }, []
+    | AddThing   -> { m with ToDoList = m.State :: m.ToDoList }, [ SetAsyncCmd m.ToDoList ]
+    | DelThing   -> { m with ToDoList = List.Empty }, []
+
+
+let bindings () =
+    [ "Count"     |> Binding.oneWay (fun m -> m.Count)
+      "SetCount"  |> Binding.cmd SetCount
+      "SetMinus"  |> Binding.cmd SetMinus
+      "State"     |> Binding.twoWay ((fun m -> m.State.ToString()), SetState)
+      "ToDoList"  |> Binding.oneWay (fun m -> m.ToDoList)
+      "AddThing"  |> Binding.cmd AddThing
+      "DelThing"  |> Binding.cmd DelThing ]
+
 
 let Run window =
     Program.mkProgramWpf
@@ -42,4 +67,3 @@ let Run window =
         update
         bindings
     |> Program.startElmishLoop ElmConfig.Default window
-
